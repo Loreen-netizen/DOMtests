a374dc98131dfd2407a02220650abf7417985b35
Loreen-netizen <loreenmuusha@gmail.com>
2020-05-15 15:44:13 +0200

    text-bill widget tests

---
+++ text-bill3.tests.js
@@ -0,0 +1,31 @@
+describe("textInputBill", function(){
+it ("should charge R2.75 for each new call", function(){
+
+
+});
+it ("should update calls total new call", function(){
+
+
+});
+it ("should charge R0.75 for each new sms", function(){
+
+
+});
+it ("should update sms total for each new sms", function(){
+
+
+});
+it ("should update overall total for each new call or sms", function(){
+
+
+});
+it ("should change class color to orange when warning level of R30 is reached", function(){
+
+
+});
+it ("should change class color to red when critical level of R50 is reached", function(){
+
+
+});
+
+});
